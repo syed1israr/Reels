01031a7ccc832c95636b68941bd19b8ddf33765f
Israr <syedfaraz173@gmail.com>
2025-02-18 18:30:41 +0530

    Added to User and Videos Schemas

---
+++ Models/User.ts
@@ -0,0 +1,29 @@
+import mongoose, { model, models, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface Iuser{
+    email : string;
+    password : string;
+    _id? : mongoose.Types.ObjectId;
+    createdAt ?: Date;  
+    updatedAt ?: Date;  
+}
+
+const userSchema = new Schema<Iuser>({
+    email : { type : String, required : true, unique : true},
+    password : { type : String, required : true}
+},{timestamps : true});
+
+
+userSchema.pre("save", async function (next) {
+    if( this.isModified("password") ){
+        this.password = await bcrypt.hash(this.password,10)
+    }
+    next();
+
+})
+
+const User =  models?.User  || model<Iuser>("User",userSchema)
+
+
+export default User;
---
+++ Models/Video.ts
@@ -0,0 +1,46 @@
+import mongoose, { model, models, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+export const Dimensions = {
+    width : 1080,
+    height : 1920
+} as const;
+
+export interface IVideo{
+    _id ? : mongoose.Types.ObjectId;
+    title : string;
+    description : string;
+    videoUrl : string;
+    thumbnailUrl : string;
+    controls ?: boolean;
+    transformation ?: {
+        height : number
+        width : number
+        quality ?: number
+    },
+    createdAt ?: Date;  
+    updatedAt ?: Date;  
+};
+
+
+
+const videoSchema = new Schema<IVideo>({
+    title : { type : String , required : true},
+    description : { type : String , required : true},
+    videoUrl : { type : String , required : true},
+    thumbnailUrl : { type : String , required : false},
+    controls : { type : Boolean , default : true},
+    transformation : {
+        height :  { type : Number , default : Dimensions.height},
+        width :  { type : Number , default : Dimensions.width},
+        quality :  { type : Number , min:1, max:100},
+        
+    }
+},{timestamps : true});
+
+
+const Video = models?.Video || model<IVideo>("Video",videoSchema);
+
+
+export default Video;
