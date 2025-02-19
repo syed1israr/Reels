3654efd4a064c6efe5bb7301491ba3947931d058
Israr <syedfaraz173@gmail.com>
2025-02-20 01:10:19 +0530

    Implemented NextAuth

--- lib/DB.ts
+++ lib/DB.ts
@@ -24,7 +24,6 @@ export async function connectToDatabase() {
     }
 
     try {cached.conn = await cached.promise;} 
-    catch (error) {cached.promise = null;throw new Error(`Error in DB File 
-        ${error}`); }
+    catch (error) {cached.promise = null;throw new Error(`Error in DB File ${error}`); }
     return cached.conn;
 }
